167affc2f4b1f5b92938833c94665fa6e29a7c59
unicellye-eng <unicell.ye@gmail.com>
2026-01-23 03:29:25 +0300

    Add files via upload

---
+++ carriers.js
@@ -0,0 +1,23 @@
+// carriers.js
+// ملف تعريف شركات الاتصالات (بطاقات الأكواد)
+
+window.CARRIERS = [
+  {
+    key: "yemen",
+    name: "Yemen Mobile",
+    logo: "media/carriers/yemen.webp",
+    number: "1100"
+  },
+  {
+    key: "sabafon",
+    name: "Sabafon",
+    logo: "media/carriers/sabafon.webp",
+    number: "111"
+  },
+  {
+    key: "you",
+    name: "YOU",
+    logo: "media/carriers/you.webp",
+    number: "1017"
+  }
+];
